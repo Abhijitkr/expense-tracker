5c0dcdc0f0fb2de5385824051a5fd957ea30e217
AbhijitKr <abhijitkr600@gmail.com>
2024-02-10 11:43:38 +0530

    Initial Commit with all the required packages like Chakra UI, react-apexcharts etc.,

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container bg="green.500" textColor="white">
+      test
+    </Container>
+  );
+}
+
+export default App;
